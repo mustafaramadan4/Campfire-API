43118b15273b35e651b5578c7e6ae916c4437dbd
Mustafa Ramadan <ramadan.m@husky.neu.edu>
2020-08-06 13:18:29 -0700

    commititing to test for git push heroku master

--- api_handler.js
+++ api_handler.js
@@ -38,7 +38,10 @@ const server = new ApolloServer({
     console.log(error);
     return error;
   },
+  playground: true,
+  introspection: true,
 });
+
 function installHandler(app) {
   const enableCors = (process.env.ENABLE_CORS || 'true') === 'true';
   console.log('CORS setting:', enableCors);
